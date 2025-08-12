a811502e202b9ad119d5f98b3e3d13ed29cfc145
Abijoshi <abijoshi86@gmail.com>
2025-08-12 11:40:05 +0530

    Fix: add CSS and JS files

--- script.js
+++ script.js
@@ -17,8 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
     });
 });
